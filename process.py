741a4d34fb7e7331f846999fef5540a6f6137ca1
taylanates24 <taylan.ates417311@gmail.com>
2023-03-07 21:00:45 +0000

    Calculates IOUs in a vectorized way

--- process.py
+++ process.py
@@ -51,43 +51,46 @@ def process_unmatched_trackers(unmatched_trks, x_box, tracker_list):
         tmp_trk.no_losses += 1
         tmp_trk.predict_only()
         xx = tmp_trk.x_state
-        xx = xx.T[0].tolist()
+        xx = xx.T[0]
         xx =[xx[0], xx[2], xx[4], xx[6]]
         tmp_trk.box =xx
         x_box[trk_idx] = xx
 
+def calc_iou(bboxes1, bboxes2):
+    x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
+    x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
+    xA = np.maximum(x11, np.transpose(x21))
+    yA = np.maximum(y11, np.transpose(y21))
+    xB = np.minimum(x12, np.transpose(x22))
+    yB = np.minimum(y12, np.transpose(y22))
+    interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
+    boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
+    boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
+    
+    if not (boxAArea + np.transpose(boxBArea) - interArea).all():
+        return 0
+    iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
+    return iou
 
 def assign_detections_to_trackers(trackers, detections, labels, scores, iou_thrd = 0.3):
     '''
     From current list of trackers and new detections, output matched detections,
     unmatchted trackers, unmatched detections.
     '''    
-    if len(detections):
-        print('asd')
-    #z_box, labels, scores = detections
-    def calc_iou(bboxes1, bboxes2):
-        x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
-        x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
-        xA = np.maximum(x11, np.transpose(x21))
-        yA = np.maximum(y11, np.transpose(y21))
-        xB = np.minimum(x12, np.transpose(x22))
-        yB = np.minimum(y12, np.transpose(y22))
-        interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
-        boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
-        boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
-        iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
-        return iou
+
+
 
     IOU_mat= np.zeros((len(trackers),len(detections)),dtype=np.float32)
-    '''
-    if len(trackers):
-        IOU_mat = calc_iou(np.array(trackers), np.array(detections))'''
-      
+    
+    if len(trackers) and len(detections):
+        IOU_mat = calc_iou(np.array(trackers), np.array(detections))
+    
+    '''  
     for t,trk in enumerate(trackers):
         #trk = convert_to_cv2bbox(trk) 
         for d,det in enumerate(detections):
          #   det = convert_to_cv2bbox(det)
-            IOU_mat[t,d] = box_iou2(trk,det)
+            IOU_mat[t,d] = box_iou2(trk,det)'''
     
     # Produces matches       
     # Solve the maximizing the sum of IOU assignment problem using the
