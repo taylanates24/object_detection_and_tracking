839515f708a520765a5ec979f4cdf87e8776e7de
taylanates24 <taylan.ates417311@gmail.com>
2023-03-09 22:43:54 +0000

    Del unnecessary lines tracker.py

--- tracker.py
+++ tracker.py
@@ -3,23 +3,19 @@ from numpy import dot
 from scipy.linalg import inv, block_diag
 from typing import List
 
-class Tracker(): # class for Kalman Filter-based tracker
+class Tracker():
     def __init__(self):
-        # Initialize parametes for tracker (history)
-        self.id = 0  # tracker's id 
-        self.box = [] # list to store the coordinates for a bounding box 
-        self.hits = 0 # number of detection matches
-        self.no_losses = 0 # number of unmatched tracks (track loss)
+
+        self.id = 0 
+        self.box = [] 
+        self.hits = 0 
+        self.no_losses = 0 
         self.label = None
         self.score = None
-        # Initialize parameters for Kalman Filtering
-        # The state is the (x, y) coordinates of the detection box
-        # state: [up, up_dot, left, left_dot, down, down_dot, right, right_dot]
-        # or[up, up_dot, left, left_dot, height, height_dot, width, width_dot]
+
         self.x_state=[] 
-        self.dt = 1.   # time interval
+        self.dt = 1.
         
-        # Process matrix, assuming constant velocity model
         self.F = np.array([[1, self.dt, 0,  0,  0,  0,  0, 0],
                            [0, 1,  0,  0,  0,  0,  0, 0],
                            [0, 0,  1,  self.dt, 0,  0,  0, 0],
@@ -29,26 +25,21 @@ class Tracker(): # class for Kalman Filter-based tracker
                            [0, 0,  0,  0,  0,  0,  1, self.dt],
                            [0, 0,  0,  0,  0,  0,  0,  1]])
         
-        # Measurement matrix, assuming we can only measure the coordinates
-        
+
         self.H = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
                            [0, 0, 1, 0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 1, 0, 0, 0], 
                            [0, 0, 0, 0, 0, 0, 1, 0]])
         
-        
-        # Initialize the state covariance
+
         self.L = 10.0
         self.P = np.diag(self.L*np.ones(8))
         
-        
-        # Initialize the process covariance
         self.Q_comp_mat = np.array([[self.dt**4/4., self.dt**3/2.],
                                     [self.dt**3/2., self.dt**2]])
         self.Q = block_diag(self.Q_comp_mat, self.Q_comp_mat, 
                             self.Q_comp_mat, self.Q_comp_mat)
         
-        # Initialize the measurement covariance
         self.R_scaler = 1.0
         self.R_diag_array = self.R_scaler * np.array([self.L, self.L, self.L, self.L])
         self.R = np.diag(self.R_diag_array)
@@ -59,26 +50,21 @@ class Tracker(): # class for Kalman Filter-based tracker
         self.R = np.diag(R_diag_array)
         
         
-        
-        
     def kalman_filter(self, z: List[int]): 
         
-        # Predict
         self.x_state = dot(self.F, self.x_state)
         self.P = dot(self.F, self.P).dot(self.F.T) + self.Q
 
-        #Update
         S = dot(self.H, self.P).dot(self.H.T) + self.R
-        K = dot(self.P, self.H.T).dot(inv(S)) # Kalman gain
-        y = z - dot(self.H, self.x_state) # residual
+        K = dot(self.P, self.H.T).dot(inv(S))
+        y = z - dot(self.H, self.x_state)
         self.x_state += dot(K, y)
         self.P = self.P - dot(K, self.H).dot(self.P)
-        self.x_state = self.x_state.astype(int) # convert to integer coordinates 
-                                     #(pixel values)
+        self.x_state = self.x_state.astype(int)
+        
         
     def predict_only(self):  
         
-        # Predict
         self.x_state = dot(self.F, self.x_state)
         self.P = dot(self.F, self.P).dot(self.F.T) + self.Q
         self.x_state = self.x_state.astype(int)
