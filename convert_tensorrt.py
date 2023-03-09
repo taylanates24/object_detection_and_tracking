299008c4bb004ab5f8ed8cc9bd3dc90846eeccc8
taylanates24 <taylan.ates417311@gmail.com>
2023-03-09 22:14:17 +0000

    Update convert_tensorrt

--- convert_tensorrt.py
+++ convert_tensorrt.py
@@ -1,17 +1,13 @@
 from argparse import ArgumentParser
-
 import torch
-from mmdet.apis import inference_detector
-
 from mmdet2trt import mmdet2trt
-from mmdet2trt.apis import create_wrap_detector
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--config', default='/workspaces/detection_and_tracking/yolox_x_8x8_300e_coco.py', help='mmdet Config file')
-    parser.add_argument('--checkpoint', default='/workspaces/detection_and_tracking/yolox_xl_epoch_329_32-2map.pth', help='mmdet Checkpoint file')
-    parser.add_argument('--save_path', default='/workspaces/detection_and_tracking/new.pth', help='tensorrt model save path')
+    parser.add_argument('--checkpoint', default='/workspaces/detection_and_tracking/yolox-xl-epoch_345-327map.pth', help='mmdet Checkpoint file')
+    parser.add_argument('--save_path', default='/workspaces/detection_and_tracking/yolox-xl-best.pth', help='tensorrt model save path')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
