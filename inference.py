7e5b5059641b7c3d96a80844763214efa0c72308
taylanates24 <taylan.ates417311@gmail.com>
2023-03-09 22:19:36 +0000

    Update inference.py only writes video

--- inference.py
+++ inference.py
@@ -5,41 +5,14 @@ import argparse
 from detector import Detector
 from process import *
 from tkinter import *
-import matplotlib
-matplotlib.use('TkAgg')
+import time
+import numpy as np
+from typing import List, Tuple
 
-def draw_boxes(img, box, thickness, class_names, label, score, colors, font=1, eps=0.01, font_size=13):
-    #alpha = 0.7
 
-    color = colors[label]
-    pos = np.array([box[1], box[0]]) - thickness
+def process_frame(img: np.ndarray, detector: Callable, frame_count: int, max_age: int, min_hits: int, 
+                  tracker_list: List, track_id_list: deque, colors: List[Tuple[int]], num_skip_frame: int):
 
-    #label_text = str(id)
-    label_text =f'{class_names[label]}'
-    
-    label_text += f'| {score:.02f}'
-    #left, top, right, bottom = int(bbox_cv2[1]), int(bbox_cv2[0]), int(bbox_cv2[3]), int(bbox_cv2[2])
-    pt1 = (int(box[1]), int(box[0]))
-    pt2 = (int(box[3]), int(box[2]))
-    cv2.rectangle(img,pt1,pt2,color,2)
-    #overlay = img.copy()        #(241,101,72)
-    text_size, _ = cv2.getTextSize(label_text, font, 0.8, 1)
-    #text_scale = int((int(box[1]) - int(box[0])) / 200)
-    text_w, text_h = text_size
-    text_w += 2
-    text_h += 2
-
-    x, y = pos
-    #cv2.rectangle(overlay, pos, (x + text_w, y + text_h), (0,0,0), -1)
-    #img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
-    cv2.putText(img, label_text, (x, y + text_h ), font, 
-            0.8, color, 1, cv2.LINE_AA)
-    return img
-
-def pipeline(img, detector, frame_count, max_age, min_hits, tracker_list, track_id_list, colors, num_skip_frame):
-    '''
-    Pipeline function for detection and tracking
-    '''
 
     if frame_count % (num_skip_frame+1) == 0:
         z_box, labels, scores = detector.detect_image(img)
@@ -53,7 +26,7 @@ def pipeline(img, detector, frame_count, max_age, min_hits, tracker_list, track_
             x_box.append(trk.box)
 
     matched, unmatched_dets, unmatched_trks \
-    = assign_detections_to_trackers(x_box, z_box, labels, scores, iou_thrd = 0.3)      
+    = assign_detections_to_trackers(x_box, z_box, iou_thrd = 0.3)      
 
     if matched.size >0:
         process_matched_detections(matched, z_box, x_box, tracker_list, labels, scores)
@@ -86,18 +59,18 @@ def pipeline(img, detector, frame_count, max_age, min_hits, tracker_list, track_
 if __name__ == "__main__":    
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--max_age', type=int, default=2, help='Number of consecutive unmatched detection before a track is deleted.')
-    parser.add_argument('--checkpoint', type=str, default='/workspaces/detection_and_tracking/yolox_xl_epoch_329_32-2map_trt.pth', 
+    parser.add_argument('--max_age', type=int, default=3, help='Number of consecutive unmatched detection before a track is deleted.')
+    parser.add_argument('--checkpoint', type=str, default='/workspaces/detection_and_tracking/yolox-xl-best.pth', 
                         help='The TensorRT checkpoint path.')
     parser.add_argument('--config', type=str, default='/workspaces/detection_and_tracking/yolox_x_8x8_300e_coco.py', 
                         help='The config path of mmdetection model.')
     parser.add_argument('--score_thr', type=int, default=0.4, help='Score threshold.')
-    parser.add_argument('--min_hits', type=int, default=1, help='Number of consecutive matches needed to establish a track.')
+    parser.add_argument('--min_hits', type=int, default=2, help='Number of consecutive matches needed to establish a track.')
     parser.add_argument('--input_video', type=str, default='/workspaces/detection_and_tracking/tokyo_1.mp4', 
                         help='The path of input video.')
     parser.add_argument('--skip_frame', type=int, default=0, help='Number of frames that only tracked, not detected by model.')
-    parser.add_argument('--save_video', type=bool, default=False, help='Saving output video.')
-    parser.add_argument('--save_video_path', type=str, default='out.mp4', help='Output video path.')
+    parser.add_argument('--save_video', type=bool, default=True, help='Saving output video.')
+    parser.add_argument('--save_video_path', type=str, default='out_best2.mp4', help='Output video path.')
     args = parser.parse_args()
     frame_count = 0
     tracker_list = []
@@ -118,23 +91,24 @@ if __name__ == "__main__":
         frame_height = int(cap.get(4))
         size = (frame_width, frame_height)
         result = cv2.VideoWriter(args.save_video_path, 
-                        cv2.VideoWriter_fourcc(*'MJPG'),
+                        cv2.VideoWriter_fourcc(*'mp4v'),
                         30, size)
     
     while(cap.isOpened()):
         ret, frame = cap.read()
-
-        image_box = pipeline(frame, detector, frame_count, args.max_age, args.min_hits, tracker_list, track_id_list, colors, args.skip_frame)
+        st = time.time()
+        image_box = process_frame(frame, detector, frame_count, args.max_age, args.min_hits, tracker_list, track_id_list, colors, args.skip_frame)
+        end = time.time()
+        print('process time: ', 1000*(end-st))
         frame_count += 1
         
         if args.save_video:
             result.write(image_box)
-            
-        cv2.imshow('frame', image_box)   
-        cv2.waitKey(20)
+        #cv2.imshow('frame', image_box)   
+        #cv2.waitKey(20)
         
     cap.release()
     
-    if args.save_vide:
+    if args.save_video:
         result.release()
 
