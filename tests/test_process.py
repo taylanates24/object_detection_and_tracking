3b5967ec983ae2eaa7e7c81ee79dafac49a76bfd
taylanates24 <taylan.ates417311@gmail.com>
2023-03-10 20:25:04 +0000

    Update test_process

--- tests/test_process.py
+++ tests/test_process.py
@@ -9,6 +9,7 @@ from tracker import Tracker
 class TestProcessUnmatchedDetections(unittest.TestCase):
 
     def setUp(self):
+        
         self.z_box = [np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8])]
         self.unmatched_dets = np.array([0, 1])
         self.x_box = []
@@ -20,15 +21,18 @@ class TestProcessUnmatchedDetections(unittest.TestCase):
     def test_process_unmatched_detections(self):
         process_unmatched_detections(self.z_box, self.unmatched_dets, self.x_box, self.tracker_list,
                                      self.track_id_list, self.labels, self.scores)
+        
         self.assertEqual(len(self.x_box), 2)
         self.assertEqual(len(self.tracker_list), 2)
         self.assertEqual(len(self.track_id_list), 1)
+        
         self.assertEqual(self.tracker_list[0].id, 0)
         self.assertEqual(self.tracker_list[1].id, 1)
         self.assertEqual(self.tracker_list[0].label, 1)
         self.assertEqual(self.tracker_list[1].label, 2)
         self.assertEqual(self.tracker_list[0].score, 0.6)
         self.assertEqual(self.tracker_list[1].score, 0.8)
+        
         self.assertTrue(np.array_equal(self.x_box[0], [1, 2, 3, 4]))
         self.assertTrue(np.array_equal(self.x_box[1], [5, 6, 7, 8]))
     
@@ -64,6 +68,7 @@ class TestProcessMatchedDetections(unittest.TestCase):
         self.assertEqual(tracker_list[1].score, 0.8)
 
 class TestProcessUnmatchedTrackers(unittest.TestCase):
+    
     def test_process_unmatched_trackers(self):
         # create some dummy data
         unmatched_trks = np.array([0, 1, 2]) # unmatched tracker indices
