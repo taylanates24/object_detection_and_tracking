74857d3166d20b4117b9e2172a952a499a86fa14
taylanates24 <taylan.ates417311@gmail.com>
2023-03-10 19:48:48 +0000

    Add docstring

--- detector.py
+++ detector.py
@@ -10,26 +10,26 @@ class Detector:
                  model_config_path: str='/workspaces/detection_and_tracking/yolox_x_8x8_300e_coco.py',
                  device: str='cuda:0',
                  score_thr: float=0.4) -> None:
-        """_summary_
+        """The class in whic the object detection phase occurs. The TensorRT module runs in this class.
 
         Args:
-            checkpoint_path (str, optional): _description_. Defaults to '/workspaces/detection_and_tracking/yolox_xl_epoch_329_32-2map_trt.pth'.
-            model_config_path (str, optional): _description_. Defaults to '/workspaces/detection_and_tracking/yolox_x_8x8_300e_coco.py'.
-            device (_type_, optional): _description_. Defaults to 'cuda:0'.
-            score_thr (float, optional): _description_. Defaults to 0.4.
+            checkpoint_path (str, optional): The checkpoint path of TensorRT module. Defaults to '/workspaces/detection_and_tracking/yolox_xl_epoch_329_32-2map_trt.pth'.
+            model_config_path (str, optional): The config path of the model. Defaults to '/workspaces/detection_and_tracking/yolox_x_8x8_300e_coco.py'.
+            device (_type_, optional): The device where object detection occurs. Defaults to 'cuda:0'.
+            score_thr (float, optional): The confidence threshold of the TensorRT module. Defaults to 0.4.
         """
         
         self.detector = create_wrap_detector(checkpoint_path, model_config_path, device)
         self.score_thr = score_thr
 
     def detect_image(self, image: np.ndarray) -> Tuple:
-        """_summary_
+        """The function which detects the objects in an image.
 
         Args:
-            image (np.ndarray): _description_
+            image (np.ndarray): The input image.
 
         Returns:
-            Tuple: _description_
+            Tuple: Bounding boxes, labels and confidence scores.
         """
         result = inference_detector(self.detector, image)
         
